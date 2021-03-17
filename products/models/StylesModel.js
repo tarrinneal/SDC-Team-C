33783df202f8aead813285f4aab57f4f2d7398f6
tarrinneal <tarrinneal@gmail.com>
2021-03-17 16:09:48 -0700

    notes

--- products/models/StylesModel.js
+++ products/models/StylesModel.js
@@ -50,6 +50,31 @@ module.exports = {
 //   ],
 }
 
+// USE sdc_photos;
+
+// cqlsh DBUSER -f relative/path/to/cql
+
+
+// COPY listings_by_id (
+// share_id,
+// name,
+// rating,
+// reviews,
+// listing,
+// photo_id,
+// description,
+// url
+// )
+// FROM 'database/monsterListing3.csv'
+// WITH DELIMITER=','
+// AND HEADER=TRUE;
+
+// http://blog.sws9f.org/nosql/2016/02/11/import-csv-to-cassandra.html#:~:text=Import%20CSV%20to%20Cassandra%20Two%20possible%20way%20to,should%20prepare%20sstable-format%20files%20with%20CQLSSTableWriter%20before%20import
+
+// copy from csv file import somehow.
+// cql file? that can run commands including pulling
+
+
 // product = {
 //   product_id: 'int',
 //   name: 'varchar',
