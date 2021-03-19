9fb6b006f4a008cb824e9b2e9f5219e18c4a7c7c
tarrinneal <tarrinneal@gmail.com>
2021-03-19 14:08:24 -0700

    working solution to add features list

---
+++ products/addFeatures.js
@@ -0,0 +1,73 @@
+const ExpressCassandra = require('express-cassandra');
+const fs = require('fs');
+var LineByLineReader = require('line-by-line');
+
+
+
+
+module.exports = {
+  init: (models) => {
+    let prod = models.instance.Products;
+
+    let header = false;
+    let q = [];
+    let counter = 0;
+
+    lr = new LineByLineReader('/home/bargle/hackreactor/SDC-Team-C/products/features.csv');
+
+    lr.on('line', function (line) {
+      let row = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+
+      for (let i = 0; i < row.length; i++) {
+        if (row[i] === 'null') {
+          row[i] = ''
+        } else {
+          if (row[i].substr(0, 1) === '"' && row[i].substr(row[i].length - 1) === '"') {
+            row[i] = row[i].substr(1, row[i].length - 2);
+          }
+        }
+      }
+
+      if (!header) {
+        header = !header
+      } else {
+
+        let id = parseInt(row[1], 10)
+
+        let u = prod.update(
+          {product_id: id},
+          {
+            features:{'$add': [row[2]]},
+            feature_values:{'$add': [row[3]]},
+          },
+          {return_query: true}
+          )
+          q.push(u)
+          if (q.length > 50) {
+            lr.pause();
+
+            models.doBatch(q, function(err){
+            if(err) {
+              console.error(err)
+            } else {
+              console.log('added?', counter++)
+              q = [];
+              lr.resume();
+            }
+          })
+        }
+      }
+    });
+
+    lr.on('end', (line) => {
+      models.doBatch(q, function(err){
+        if(err) {
+          console.error(err)
+        } else {
+              console.log('complete', counter++)
+          q = [];
+        }
+      })
+    })
+  }
+}
--- products/db.js
+++ products/db.js
@@ -1,22 +1,132 @@
-const models = require('express-cassandra');
+const ExpressCassandra = require('express-cassandra');
+const fs = require('fs');
+const readline = require('readline');
+const addFeatures = require('./addFeatures')
 
-models.setDirectory( __dirname + '/models').bind(
-  {
-      clientOptions: {
-          contactPoints: ['127.0.0.1'],
-          protocolOptions: { port: 9042 },
-          keyspace: 'products',
-          queryOptions: {consistency: models.consistencies.one}
+var models = ExpressCassandra.createClient({
+    clientOptions: {
+        contactPoints: ['127.0.0.1'],
+        protocolOptions: { port: 9042 },
+        keyspace: 'products',
+        queryOptions: {consistency: ExpressCassandra.consistencies.one}
+    },
+    ormOptions: {
+        defaultReplicationStrategy : {
+            class: 'SimpleStrategy',
+            replication_factor: 1
+        },
+        migration: 'safe',
+    }
+});
+
+var ProductModel = models.loadSchema('Products', {
+    fields: {
+      product_id: {
+        type: "int"
       },
-      ormOptions: {
-          defaultReplicationStrategy : {
-              class: 'SimpleStrategy',
-              replication_factor: 1
-          },
-          migration: 'safe'
+      name: {
+        type: "varchar"
+      },
+      slogan: {
+        type: "varchar"
+      },
+      description: {
+        type: "varchar"
+      },
+      category: {
+        type: "varchar"
+      },
+      default_price: {
+        type: "int"
+      },
+      created_at: "timestamp",
+      updated_at: "timestamp",
+      features: {
+        type: "list",
+        typeDef: "<varchar>",
+      },
+      feature_values: {
+        type: "list",
+        typeDef: "<varchar>",
+      },
+      related_products: {
+        type: "list",
+        typeDef: "<int>",
+      },
+    },
+    key:["product_id"],
+  //   required: [
+  //     "name",
+  //     "category",
+  //     "created_at",
+  //     "product_id"
+  //   ],
+  });
+
+  var StyleModel = models.loadSchema('Styles', {
+    fields: {
+      style_id: {
+        type: "int"
+      },
+      product_id: {
+        type: "int"
+      },
+      name: {
+        type: "varchar"
+      },
+      original_price: {
+        type: "varchar"
+      },
+      original_price: {
+        type: "varchar",
+        default: null
+      },
+      "default?": {
+        type: "boolean",
+        default: false
+      },
+      sku_id: {
+        type: "list",
+        typeDef: "<int>"
+      },
+      sku_size: {
+        type: "list",
+        typeDef: "<varchar>"
+      },
+      sku_quantity: {
+        type: "list",
+        typeDef: "<int>"
+      },
+      photo_urls: {
+        type: "list",
+        typeDef: "<varchar>"
+      },
+      photo_thumbnail_urls: {
+        type: "list",
+        typeDef: "<varchar>"
       }
-  },
-  function(err) {
-      if(err) throw err;
-  }
-);
+    },
+    key:["product_id", "style_id"],
+  //   required: [
+  //     "name",
+  //     "category",
+  //     "created_at",
+  //     "product_id"
+  //   ],
+  });
+
+// addFeatures.init(models)
+
+
+ProductModel.syncDB(function(err, result) {
+    if (err) throw err;
+    console.log('changed?', result)
+});
+
+StyleModel.syncDB(function(err, result) {
+    if (err) throw err;
+    console.log('changed?', result)
+});
+
+
+// COPY products."Products"(product_id, name, slogan, description, category, default_price) FROM '/home/bargle/hackreactor/SDC-Team-C/products/product.csv' WITH DELIMITER=',' AND HEADER=TRUE ;
